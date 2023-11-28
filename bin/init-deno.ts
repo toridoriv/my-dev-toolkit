db61f6d045040c13238fb080d071ceae328892ca
Victoria Rodriguez <vrodriguezfe@icloud.com>
2023-11-27 22:52:46 -0300

    feat(bin/init-deno): Add license to new project

--- bin/init-deno.ts
+++ bin/init-deno.ts
@@ -5,16 +5,25 @@ export default defineCommandOptions({
   description: "Initiates a Deno project.",
   arguments: [{ name: "path", type: "string" }],
   async action(_options, path) {
-    const vscodePath = path + "/.vscode/";
+    if (!path.endsWith("/")) {
+      path = path + "/";
+    }
+
+    const vscodePath = path + ".vscode/";
     const extensions = await getExtensionsFile();
     const settings = await getSettingsFile();
+    const license = await getLicenseFile();
 
     extensions.filename = vscodePath + extensions.filename;
     settings.filename = vscodePath + settings.filename;
+    license.filename = path + license.filename;
 
     Deno.mkdirSync(vscodePath, { recursive: true });
     Deno.writeTextFileSync(extensions.filename, extensions.content);
     Deno.writeTextFileSync(settings.filename, settings.content);
+    Deno.writeTextFileSync(license.filename, license.content);
+
+    globalThis.logger.info(`New project created at ${path}`);
   },
 });
 
@@ -31,6 +40,15 @@ async function getExtensionsFile() {
   const filename = "extensions.json";
   const response = await fetch(
     `https://raw.githubusercontent.com/toridoriv/my-dev-toolkit/main/.vscode/${filename}`,
+  );
+
+  return { filename, content: await response.text() };
+}
+
+async function getLicenseFile() {
+  const filename = "LICENSE";
+  const response = await fetch(
+    `https://raw.githubusercontent.com/toridoriv/my-dev-toolkit/main/${filename}`,
   );
 
   return { filename, content: await response.text() };
